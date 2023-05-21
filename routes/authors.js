2001c9bc04c6111e0ecf0d533c8a377202feb6aa
TecHeReTiC <talex73@mail.ru>
2023-05-21 12:25:32 +0500

    Case-insensitive search seems to be implemented

--- routes/authors.js
+++ routes/authors.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/authors');
+const { Sequelize, Op } = require('sequelize');
 
 // Get all authors
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    try {
+        let authors;
+        if (req.query.pattern ) {
+            let reg = req.query.pattern.toLowerCase();
+            authors = await Author.findAll({
+                    where: {
+                        [Op.and]: [
+                            Sequelize.where(
+                                Sequelize.fn('lower', Sequelize.col('name')),
+                                {
+                                    [Op.like]: `%${reg}%`
+                                }
+                            )
+                        ]
+                    }
+                });
+        } else {
+            authors = await Author.findAll();
+        }
+
+
+        res.render('authors/index', { authors });
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/');
+    }
 });
 
 // New authors form
@@ -20,7 +46,6 @@ router.post('/', async (req, res) => {
             age: +req.body.age
         });
         console.log(newAuthor.toJSON());
-        console.log('on new post');
         res.redirect('/authors'); // then to author profile page
         // res.redirect(`/authors/${newAuthor.id}`); // then to author profile page
     } catch (err) {
